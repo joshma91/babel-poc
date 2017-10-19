9e30dc1804f3a0c17f5a4c561921c7499c889a42
Joshma91 <josh.ma91@gmail.com>
2017-10-19 16:51:14 -0700

    fixed migration

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,9 +1,11 @@
 var TranslationContract = artifacts.require("./TranslationContract.sol");
 var TranslationUtils = artifacts.require("./TranslationUtils.sol");
 var StringUtils = artifacts.require("./StringUtils.sol");
+var SimpleStorage = artifacts.require("./SimpleStorage.sol");
 
 module.exports = function(deployer) {
  
+  deployer.deploy(SimpleStorage);
   deployer.deploy(TranslationUtils);
   deployer.deploy(StringUtils);
   deployer.link(StringUtils, TranslationContract);
