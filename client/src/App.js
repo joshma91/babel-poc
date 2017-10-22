80bc5f4b697d35dc676b1afb72792bb1d14a821f
Adrian Li <li.adrianmc@gmail.com>
2017-10-22 13:40:19 -0700

    integrate web3 and contract instance with react app

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,42 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+import getWeb3 from "./utils/getWeb3";
+import {
+  getAccounts,
+  getTranslationInstance,
+} from "./utils/translationContract";
 
 class App extends Component {
+  state = { web3: null, contractInstance: null, accounts: null };
+
+  componentWillMount = async () => {
+    try {
+      const web3 = await getWeb3();
+      const contractInstance = await getTranslationInstance(web3);
+      const accounts = await getAccounts(web3);
+
+      this.setState({ web3, contractInstance, accounts });
+    } catch (error) {
+      alert(`Error finding web3.`, error);
+      console.log(error)
+    }
+  };
+
   render() {
+    const { web3, contractInstance, accounts } = this.state;
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Babel-PoC</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div>
+          <h2>Ready State</h2>
+          <div>web3: {Boolean(web3).toString()}</div>
+          <div>contractInstance: {Boolean(contractInstance).toString()}</div>
+          <div>accounts: {Boolean(accounts).toString()}</div>
+        </div>
       </div>
     );
   }
