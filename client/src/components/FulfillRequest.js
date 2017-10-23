6cdafd25c0b14caa6767190b27b2241be02d1acf
Adrian Li <li.adrianmc@gmail.com>
2017-10-22 17:54:54 -0700

    add fulfill request component

---
+++ client/src/components/FulfillRequest.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default class extends React.Component {
+  state = {};
+  render() {
+    return (
+      <div>
+        <h2>3. Fulfill a translation request.</h2>
+      </div>
+    );
+  }
+}
