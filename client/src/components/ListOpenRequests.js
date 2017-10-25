88736fb2d5e0385897949dbf761bfeef51c52d64
Joshma91 <josh.ma91@gmail.com>
2017-10-25 09:36:44 -0700

    merge with master

--- client/src/components/ListOpenRequests.js
+++ client/src/components/ListOpenRequests.js
@@ -1,12 +1,24 @@
 import React from "react";
 
 export default class extends React.Component {
-  state = { openRequests: [] };
+  state = { openRequestStrings: [] };
 
   getOpenRequests = async () => {
     const { contractInstance, web3 } = this.props;
-    const results = await contractInstance.getAllOpenRequests.call();
-    this.setState({ openRequests: results.map(x => x.toNumber())})
+    const result = await contractInstance.getAllOpenRequests.call();
+    const openRequestIds = result.map(x => x.toNumber());
+    // const str1 = await contractInstance.getRequestString(0);
+    // const str2 = await contractInstance.getRequestString(1);
+    let openRequestStrings = [];
+
+    for (let i=0; i<openRequestIds.length; i++){
+      const temp = await contractInstance.getRequestString(openRequestIds[i]);
+      openRequestStrings.push(temp);
+    }
+
+    console.log('hey')
+    console.log(openRequestStrings)
+    this.setState({ openRequestStrings });
   };
 
   render() {
@@ -14,8 +26,8 @@ export default class extends React.Component {
       <div>
         <h2>2. List all open translation requests.</h2>
         <button onClick={this.getOpenRequests}>Get Open Requests</button>
-        {this.state.openRequests
-          ? <ul>{this.state.openRequests.map(x => <li>{x}</li>)}</ul>
+        {this.state.openRequestStrings
+          ? <ul>{this.state.openRequestStrings.map(x => <li>{x}</li>)}</ul>
           : null}
       </div>
     );
